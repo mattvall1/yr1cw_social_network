73bc2109a89a8da49eb159887abcd0ff79f64871
Matthew Vallance <matthew.vallance@outlook.com>
2022-12-28 17:03:33 +0000

    Continued work on friend reccomendation

--- classes.py
+++ classes.py
@@ -6,6 +6,7 @@
 import os
 import numpy
 
+
 class Data:
     def __init__(self):
         self.__fileName = ''
@@ -100,6 +101,7 @@ class Friends:
     def __init__(self):
         self.common_friends = []
 
+    # Can get data through inheritance perhaps?
     def get_common_friends(self, data):
         split_names = []
         # print(data): ['Adam Bob', 'Bob Amir', 'Bob Mia', 'Chris Zia', 'Mia Amir', 'Liz']
@@ -110,22 +112,41 @@ class Friends:
 
         # print(split_names): [['Adam', 'Bob'], ['Bob', 'Amir'], ['Bob', 'Mia'], ['Chris', 'Zia'], ['Mia', 'Amir'], ['Liz']]
 
-        # Reformat array
-        individual_names = sum(split_names, [])
-
-        # Remove duplicate entries and sort alphabetically
-        individual_names = sorted(numpy.unique(individual_names))
+        # Reformat array then remove duplicate entries and sort alphabetically
+        individual_names_unsorted = sum(split_names, [])
+        individual_names = sorted(numpy.unique(individual_names_unsorted))
+        # print(individual_names): ['Adam', 'Amir', 'Bob', 'Chris', 'Liz', 'Mia', 'Zia']
 
         # Run through array
-        for item in individual_names:
-            print(item)
+        for user in individual_names: # For EVERY user, we need to find how many friends they have in common with EVERY other user
+            # Start by creating a list of friends for each user
+            users_friends = []
+            for item in split_names:
+                # print(item)
+
+                # Use try statement to avoid exception handling
+                try:
+                    # Check first item against user to get users friends and add to list
+                    if item[0] == user:
+                        users_friends.append(item[1])
+                    elif item[1] == user:
+                        users_friends.append(item[0])
+
+                except IndexError:
+                    users_friends.append([])
+
+            print(users_friends)
+            # Add temporary variable to list
+            self.common_friends.append(users_friends)
+            # break
+        # print(self.common_friends)
 
         return self.common_friends
         # Example of what common friends should look like:
         #       Adam | Amir | Bob | Chris | Mia | Zia | Liz <-- "Columns" of array
-        # Adam ->[1,1,0,0,0,0,0]
+        # Adam ->[1,1,0,0,0,0,0] # Adam has 1 friend in common with amir (it's bob)
         # Amir ->[1,2,1,0,1,0,0]
-        # Bob ->[0,1,3,0,1,0,0]
+        # Bob ->[0,1,3,0,1,0,0] # Bob has 1 friend in common with amir
         # Chris->[0,0,0,1,0,0,0]
         # Mia ->[1,1,1,0,2,0,0]
         # Zia ->[0,0,0,0,0,1,0]
