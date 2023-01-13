c3129b94fedcc12d799eb53eff4d7eb94ea145d1
Matthew Vallance <matthew.vallance@outlook.com>
2023-01-13 16:10:14 +0000

    Added legacy files

---
+++ old_files/classes_11_01_23.py
@@ -0,0 +1,151 @@
+"""
+    Author: Matthew Vallance 001225832
+    Purpose: Holder file for all OOP classes
+    Date: 04/12/22
+"""
+import os
+import numpy
+
+
+class Data:
+    def __init__(self):
+        self.__fileName = ''
+        self.total_users = 0
+
+    def get_data(self, __fileName):
+        # Add file extension to fileName
+        __fileName = __fileName + '.txt'
+
+        # Get a list of files in the data directory
+        data_file_list = os.listdir('data')
+        # Check datafile exists against dir list
+        if __fileName in data_file_list:
+            # Code to open file (use: nw_data1)
+            with open('data/' + __fileName, 'r') as f:
+                users = f.readlines()
+                # Remove line endings from users list
+                users_cleaned = []
+                count = 0
+                for user in users:
+                    if count == 0:
+                        # Get total users
+                        self.total_users = int(users[0])
+                    else:
+                        users_cleaned.append(user.replace("\n", ""))
+                    count += 1
+            return users_cleaned
+        else:
+            return False
+
+    def get_user(self):
+        return self
+
+    def set_data(self):
+        return self
+
+    def set_user(self):
+        return self
+
+
+class SignUp:
+    def __init__(self):
+        self.user_id = []
+        self.username = []
+
+    def get_current_users(self):
+        return self
+
+    def set_user_id(self):
+        return self
+
+    def set_current_user(self):
+        return self
+
+    def get_user_id(self):
+        return self
+
+
+class SignIn:
+    def __init__(self):
+        self.user_id = []
+        self.username = []
+
+    def get_user(self):
+        return self
+
+    def set_user(self):
+        return self
+
+
+class User:
+    def __init__(self, name, age):
+        self.__name = ''
+        self.__age = 0
+        self.set_name(name)
+        self.set_age(age)
+
+    def get_friends(self):
+        return self
+
+    def set_friends(self):
+        return self
+
+    def add_friend(self):
+        return self
+
+    def remove_friend(self):
+        return self
+
+
+class Friends:
+    def __init__(self):
+        self.common_friends_matrix = []
+
+    # Can get data through inheritance perhaps?
+    def get_common_friends(self, data):
+        # Setup variables
+        split_names = []
+        all_users_friends = []
+
+        for item in data:
+            # Split list into individual names
+            split_names.append(item.split(' '))
+
+        # Reformat array then remove duplicate entries and sort alphabetically
+        individual_names_unsorted = sum(split_names, [])
+        individual_names = sorted(numpy.unique(individual_names_unsorted))
+
+        # Run through array
+        for user in individual_names:
+            # Start by creating a set of friends for each user
+            users_friends = set()
+            for item in split_names:
+                # Exception handling, if item[1] does not exist, continue
+                try:
+                    # Check first item against user to get users friends and add to list
+                    if item[0] == user:
+                        users_friends.add(item[1])
+                    elif item[1] == user:
+                        users_friends.add(item[0])
+                except IndexError:
+                    continue
+
+            # Create a list of users and their friends
+            all_users_friends.append([user, users_friends])
+
+        # Run through the list of users and their friends
+        for user in all_users_friends:
+            common_friends = []
+            user_to_compare_friends = user[1]
+
+            # Run through the list of users and their friends again, to work out common friends for each other user
+            for friend in all_users_friends:
+                user_to_compare_friends_2 = friend[1]
+
+                # Intersection of the two sets of friends - gets a count of the common friends
+                friends_intersection = user_to_compare_friends & user_to_compare_friends_2
+                common_friends.append(len(friends_intersection))
+            # Put the commons friends matrix into an appropriate data structure
+            self.common_friends_matrix.append([user[0], common_friends])
+
+        return self.common_friends_matrix
---
+++ old_files/main_11_01_23.py
@@ -0,0 +1,74 @@
+"""
+    Author: Matthew Vallance 001225832
+    Purpose: Main file for running the social network
+    Date: 04/12/22
+"""
+import classes
+import model
+
+
+def main():
+    selected_menu_option = 0
+    valid_filename = 0
+
+    # Initialise social network routine
+    data_retrieval = classes.Data()
+
+    # Ask user to input valid filename, then get data
+    while valid_filename == 0:
+        data_file = input("Insert data filename (or 'n' to quit): ")
+        if data_file == "n":
+            exit()
+        social_nw = data_retrieval.get_data("nw_data1") # REMOVE FILENAME AFTER TESTING
+        if social_nw:
+            valid_filename = 1
+        else:
+            print("Invalid filename")
+
+    while selected_menu_option != 6:
+        valid_selection = 0
+
+        # Main menu
+        print("Menu: \n 1. Display social network \n 2. Display common friends \n 3. Sign in \n 4. Sign up \n 5. Add friend \n 6. Close")
+
+        while valid_selection == 0:
+            valid_menu_options = [1, 2, 3, 4, 5, 6]
+            # Check menu option is correct data type - CAN THIS BE DONE NEATLY?
+            try:
+                selected_menu_option = int(input("Select an option: "))
+                if selected_menu_option in valid_menu_options:
+                    valid_selection = 1
+                else:
+                    print("Invalid selection, try again. IFF")
+            except ValueError:
+                print('Invalid selection, try again.')
+                break
+
+        # 1. Display social network
+        if int(selected_menu_option) == 1:
+            # Pretty print connections
+            for user in social_nw:
+                print(user.replace(" ", " -> "))
+
+            # Return to main menu
+            model.return_to_menu()
+
+        # 2. Display common friends
+        elif int(selected_menu_option) == 2:
+            friends = classes.Friends()
+
+            common_friends = friends.get_common_friends(social_nw)
+            for common_friends_for_user in common_friends:
+                print(common_friends_for_user[0], " -> ", common_friends_for_user[1])
+
+            # Return to main menu
+            model.return_to_menu()
+
+
+# Run the main function
+main()
+
+'''
+----------------------------------References------------------------------------
+Exception handling - menu check: https://www.educative.io/answers/what-is-valueerror-in-python
+'''
